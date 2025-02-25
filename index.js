4fc7dbff3d19fb6a89a66973d0e661746126cc3b
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-02-25 22:11:52 +0530

    save

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ const server = http.createServer(async (req, res)=>{
         const html = await fs.readFile('./view/overview.html', 'utf-8')
         res.writeHead(200, {'Content-Type':'text/html'});
         res.end(html)
+        return
     }
     else
     {
