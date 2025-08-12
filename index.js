d09360b8ffa9eb52f95932d926b768a56a4576bb
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-08-12 08:29:20 +0000

    bicycles.ejs is not what needs for app Express app.

--- index.js
+++ index.js
@@ -4,20 +4,22 @@ const app = express()
 
 const bicycles = require('./data/data.json')
 
+app.set("view engine", "ejs")
+
 app.get("/", (req, res)=>{    
     console.log(req.Url)
-    return res.send(bicycles);
+    // return res.send(bicycles);
+    return res.render('bicycles');
 })
 
+
 app.get("/bicycle", (req, res)=>{
-    
     // console.log(req.url)
     
     console.log(req.query.id)
     // console.log(req.query.name)
     
     const bicycle = bicycles.find(b => b.id === req.query.id)
-
     return res.send(bicycle);
 })
 
