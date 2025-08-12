cc33894c091f2831dda2545c77f60d3f56e740ed
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-08-12 11:08:28 +0000

    Dynamic data addition is working now.

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const app = express()
 
 const bicycles = require('./data/data.json')
 
+app.use(express.static('public'))
+
 app.set("view engine", "ejs")
 
 app.get("/", (req, res)=>{    
     console.log(req.Url)
     // return res.send(bicycles);
-    return res.render('overview');
+    return res.render('bicycles');
 })
 
 
@@ -20,7 +22,8 @@ app.get("/bicycle", (req, res)=>{
     // console.log(req.query.name)
     
     const bicycle = bicycles.find(b => b.id === req.query.id)
-    return res.send(bicycle);
+    console.log(bicycle)
+    return res.render('overview',{bicycle});
 })
 
 app.listen(3000, ()=>{
