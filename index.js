1f70b3aca89ace041ef841021282670315118090
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-08-12 06:15:18 +0000

    Passing string query data

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ app.get("/", (req, res)=>{
     return res.send("Assalamualaikum");
 })
 
+
+app.get("/bicycle", (req, res)=>{
+    console.log(req.query.id)
+    // console.log(req.query.name)
+    console.log(req.url)
+    return res.send("/bicycle page");
+})
+
 app.listen(3000, ()=>{
     console.log("Alhamdulillah Started With Express @3000") 
-
 })
